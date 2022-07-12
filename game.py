52989f1aa2531e91ee5c40020df85296aac660e1
Lrich14 <lr1696@stern.nyu.edu>
2022-07-12 18:39:22 -0400

    Reorganizing the code

--- game.py
+++ game.py
@@ -2,24 +2,27 @@
 # my understanding of the exercise and functions that I need to use for the exercise.
 
 # this is the "game.py" file
+import random 
+
 "Rock, Paper, Scissors, Shoot!"
 print("Rock, Paper, Scissors, Shoot!")
 
 # processing user inputs
 user_action = input("Enter a choice (Rock, Paper, Scissors): ")
 print(user_action)
-print(type(user_action))
 
-# validate user inputs
-user_action = ("Rock, Paper, Scissors, ROCK, PAPER, SCISSORS, rock, paper, scissors")
+# validated user inputs and set up the randomized computer selections
+while True:
+    user_action = ("Rock, Paper, Scissors, ROCK, PAPER, SCISSORS, rock, paper, scissors")
 if user_action == ("Rock", "Paper", "Scissors", "ROCK", "PAPER", "SCISSORS", "rock", "paper", "scissors"):
     print(user_action)
 else:
     print("Sorry I don't understand.  Please try again")
 
-# Setting up the randomized computer selection.  I think I enter the random import here.
-import random 
-computer_choices = ("Rock", "Paper", "Scissors", "ROCK", "PAPER", "SCISSORS", "rock", "paper", "scissors")
-computer_action = random.choice(computer_choices)
+# Setting up the randomized computer selection. 
+
+    computer_choices = ("Rock", "Paper", "Scissors", "ROCK", "PAPER", "SCISSORS", "rock", "paper", "scissors")
+    computer_action = random.choice(computer_choices)
 
 
+    print(f"You chose {user_action}, computer chose {computer_action}")
