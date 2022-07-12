d1fa0cd0f5930bf436ffbd4d49d03c7f4adedb44
Lrich14 <lr1696@stern.nyu.edu>
2022-07-12 18:06:31 -0400

    Adding computer selection step

--- game.py
+++ game.py
@@ -1,15 +1,25 @@
+# Overall I found help from a guide via the RealPython [website](https://realpython.com/python-rock-paper-scissors/) which further aided in 
+# my understanding of the exercise and functions that I need to use for the exercise.
+
 # this is the "game.py" file
 "Rock, Paper, Scissors, Shoot!"
 print("Rock, Paper, Scissors, Shoot!")
 
 # processing user inputs
-x = input("Enter a choice (Rock, Paper, Scissors): ")
-print(x)
-print(type(x))
+user_action = input("Enter a choice (Rock, Paper, Scissors): ")
+print(user_action)
+print(type(user_action))
 
 # validate user inputs
-x = ("Rock, Paper, Scissors")
-if x == ("Rock", "Paper", "Scissors"):
-    print("Yes")
+user_action = ("Rock, Paper, Scissors, ROCK, PAPER, SCISSORS, rock, paper, scissors")
+if user_action == ("Rock", "Paper", "Scissors", "ROCK", "PAPER", "SCISSORS", "rock", "paper", "scissors"):
+    print(user_action)
 else:
     print("Sorry I don't understand.  Please try again")
+
+# Setting up the randomized computer selection.  I think I enter the random import here.
+import random 
+computer_choices = ("Rock", "Paper", "Scissors", "ROCK", "PAPER", "SCISSORS", "rock", "paper", "scissors")
+computer_action = random.choice(computer_choices)
+
+
